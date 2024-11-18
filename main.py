24b0eaa3bf21929d3d9aec063ec35e3db6e05a54
Arshey123 <aseemsharma1980@gmail.com>
2024-11-18 20:10:05 +0530

    lesson 12 analysis and space complexcity

---
+++ main.py
@@ -0,0 +1,13 @@
+# activity 1
+# def add(a):
+#     return a*(a+1)/2
+# print(add(10))
+# activity 2
+def arr(a):
+    sum=0
+    for i in a:
+        print("Count")
+        sum=sum+i
+    return sum
+a=[1,2,3,4,5,6,7,8,9,10]
+print(arr(a))
